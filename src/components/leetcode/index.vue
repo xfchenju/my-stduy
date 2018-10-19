39217722235eb0185a79f38e70ff0db098a9b402
陈炬 <chenju@funmart.com>
2018-10-19 17:45:48 +0800

    2018年10月19日17:45:47

--- src/components/leetcode/index.vue
+++ src/components/leetcode/index.vue
@@ -168,6 +168,257 @@
             };
             </vue-markdown>
         </div>
+        <hr/>
+        <h4>两个数组的交集 II</h4>
+        <div>
+            <label>我的解答：</label>
+            <vue-markdown>
+            var intersect = function(nums1, nums2) {
+                var res = [];
+                for(var i = 0; i < nums1.length; i++) {
+                    var numIndex = nums2.findIndex(n => n == nums1[i]);
+                    if(numIndex > -1) {
+                        res.push(nums1[i]);
+                        nums2.splice(numIndex, 1);
+                    }
+                }
+                return res;
+            };
+            </vue-markdown>
+        </div>
+        <div>
+            <label>更好的解答：</label>
+            <vue-markdown>
+            var intersect = function(nums1, nums2) {
+                var nums3 = []
+                nums1.forEach(num=>{
+                    let index = nums2.indexOf(num)
+                    if(!(index < 0)){
+                        nums3.push(nums2.splice(index,1)[0])
+                    }
+                })
+                return nums3
+            };
+            </vue-markdown>
+        </div>
+        <hr/>
+        <h4>加一</h4>
+        <div>
+            <label>我的解答：</label>
+            <vue-markdown>
+            var plusOne = function(digits) {
+                if(digits[digits.length - 1] == 9) {
+                    digits[digits.length - 1]++;
+                    for(var i = digits.length - 1; i >= 0; i--) {
+                        if(digits[i] == 10) {
+                            digits[i] = 0;
+                            if(digits[i - 1]) {
+                                digits[i - 1]++;
+                            }else {
+                                digits.splice(0, 0, 1);
+                            }
+                        }
+                    }
+                }else {
+                    digits[digits.length - 1]++;
+                }
+                return digits;
+            };
+            </vue-markdown>
+        </div>
+        <div>
+            <label>更好的解答：</label>
+            <vue-markdown>
+            const plusOne = (digits) => {
+              const len = digits.length;
+              for (let i = len - 1; i >= 0; --i) {
+                if (digits[i] < 9) {
+                  ++digits[i];
+                  return digits;
+                }
+                digits[i] = 0;
+              }
+              digits.unshift(1);
+              return digits;
+            };
+            </vue-markdown>
+        </div>
+        <hr/>
+        <h4>移动零</h4>
+        <div>
+            <label>我的解答：</label>
+            <vue-markdown>
+            var moveZeroes = function(nums) {
+                for(var i = nums.length - 1; i >= 0; i--) {
+                    if(nums[i] == 0) {
+                        nums.splice(i, 1);
+                        nums.push(0);
+                    }
+                }
+            };
+            </vue-markdown>
+        </div>
+        <div>
+            <label>更好的解答：</label>
+            <vue-markdown>
+            var moveZeroes = function (nums) {
+                let index = 0;
+                for (let i = 0; i < nums.length; i++) {
+                    if (nums[i] !== 0) {
+                        nums[index] = nums[i];
+                        index++;
+                    }
+                }
+                for (let i = index; i < nums.length; i++) {
+                    nums[i] = 0;
+                }
+            };
+            </vue-markdown>
+        </div>
+        <hr/>
+        <h4>两数之和</h4>
+        <div>
+            <label>我的解答：</label>
+            <vue-markdown>
+            var twoSum = function(nums, target) {
+                for(var i = 0; i < nums.length; i++) {
+                    for(var j = nums.length - 1; j >= 0; j--) {
+                        if((nums[i] + nums[j]) == target && i != j) {
+                            return [i, j];
+                        }
+                    }
+                }
+            };
+            </vue-markdown>
+        </div>
+        <div>
+            <label>更好的解答：</label>
+            <vue-markdown>
+            var twoSum = function(nums, target) {
+                var l= nums.length;
+                var n = {};
+                for(var i = 0; i < l; ++i) {
+                    if (n[target - nums[i]] !== undefined){
+                        return [n[target-nums[i]], i];
+                    }
+                    n[nums[i]] = i;
+                }
+            };
+            </vue-markdown>
+        </div>
+        <hr/>
+        <h4>有效的数独</h4>
+        <div>
+            <label>我的解答：</label>
+            <vue-markdown>
+            var isValidSudoku = function(board) {
+                var arr = {}
+                var arr1 = {}
+                for(var i = 0; i < board.length; i++) {
+                    var box = Math.floor(i / 3);
+                    if(!arr1[box]) {
+                        arr1[box] = {}
+                    }
+                    for(var j = 0; j < board[i].length; j++) {
+                        var box1 = Math.floor(j / 3);
+                        if(!arr1[box][box1]) {
+                            arr1[box][box1] = [];
+                        }
+                        arr1[box][box1].push(board[i][j]);
+                        if(!arr[j]) {
+                            arr[j] = {}
+                        }
+                        arr[j][i] = board[i][j];
+                        for(var k = 0; k < board[i].length; k++) {
+                            if(j != k && board[i][j] == board[i][k] && board[i][j] != '.') {
+                                return false;
+                            }
+                        }
+                    }
+                }
+                for(var i in arr) {
+                    for(var j in arr[i]) {
+                        for(var k in arr[i]) {
+                            if(j != k && arr[i][j] == arr[i][k] && arr[i][j] != '.') {
+                                return false;
+                            }
+                        }
+                    }
+                }
+                for(var i in arr1) {
+                    for(var j in arr1[i]) {
+                        for(var k in arr1[i][j]){
+                           for(var k1 in arr1[i][j]){
+                               if(k != k1 && arr1[i][j][k] == arr1[i][j][k1] && arr1[i][j][k] != '.') {
+                                   return false;
+                               }
+                           } 
+                        }
+                    }
+                }
+                return true;
+            };
+            </vue-markdown>
+        </div>
+        <div>
+            <label>更好的解答：</label>
+            <vue-markdown>
+            var isValidSudoku = function(board) {
+                var set ={};
+                for(var i=1;i<= 9;i++) set[i]=[];
+                
+                for(var i=0;i < 9;i++){
+                    for(var j=0;j < 9;j++){
+                        var s=board[i][j];
+                        if(Number(s)){
+                            var index = parseInt(i/3)*3+parseInt(j/3);
+                            for(var k=0;k < set[s].length;k++){
+                                if(i==set[s][k][0]||j==set[s][k][1]||index==set[s][k][2])
+                                    return false;
+                            }
+                            set[s].push([i,j,index]);
+                        }
+                    }
+                }
+                return true;
+                
+            };
+            </vue-markdown>
+        </div>
+        <hr/>
+        <h4>旋转图像</h4>
+        <div>
+            <label>我的解答：</label>
+            <vue-markdown>
+            var rotate = function(matrix) {
+                var len = matrix.length;
+                var arr = JSON.parse(JSON.stringify(matrix));
+                for(var i = 0; i < len; i++) {
+                    var len1 = matrix[i].length;
+                    for(var j = 0; j < len1; j++) {
+                        matrix[i][j] = arr[len1 - j - 1][i];
+                    }
+                }
+            };
+            </vue-markdown>
+        </div>
+        <div>
+            <label>更好的解答：</label>
+            <vue-markdown>
+            let rotate = function(matrix) {
+                let length = matrix.length - 1;
+                for (let y = 0; y < length / 2; y++) {
+                    for (let x = y; x < length - y; x++) {
+                        let temp = matrix[y][x];
+                        matrix[y][x] = matrix[length - x][y];
+                        matrix[length - x][y] = matrix[length - y][length - x];
+                        matrix[length - y][length - x] = matrix[x][length - y]
+                        matrix[x][length - y] = temp; 
+                    }
+                }
+            };
+            </vue-markdown>
+        </div>
     </div>
 </template>
 <script>
@@ -192,8 +443,30 @@ export default {
         var f = this.rotate1([1,2,3,4,5,6,7], 3);
         //console.log(e, f, '旋转数组');
         var g = this.singleNumber([4,1,2,1,2]);
-        console.log(g, '只出现一次的数字')
-
+        //console.log(g, '只出现一次的数字')
+        var h = this.plusOne([9,9,9,9]);
+        //console.log(h, '加一')
+        var i = this.moveZeroes([0,1,0,3,12]);
+        //console.log(i, '移动零')
+        var j = this.twoSum([1,2,3,4,5], 9);
+        //console.log(j, '两数之和')
+        var k = this.isValidSudoku([
+            [".",".",".",".","5",".",".","1","."],
+            [".","4",".","3",".",".",".",".","."],
+            [".",".",".",".",".","3",".",".","1"],
+            ["8",".",".",".",".",".",".","2","."],
+            [".",".","2",".","7",".",".",".","."],
+            [".","1","5",".",".",".",".",".","."],
+            [".",".",".",".",".","2",".",".","."],
+            [".","2",".","9",".",".",".",".","."],
+            [".",".","4",".",".",".",".",".","."]]);
+        //console.log(k, '有效的数独')
+        var l = this.rotate2([
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]);
+        console.log(l, '旋转图像')
     },
     methods: {
         // 26.删除排序数组中的重复项
@@ -299,6 +572,104 @@ export default {
             		return nums[i];
             	}
             }
+        },
+        plusOne(digits) {
+            if(digits[digits.length - 1] == 9) {
+                digits[digits.length - 1]++;
+                for(var i = digits.length - 1; i >= 0; i--) {
+                    if(digits[i] == 10) {
+                        digits[i] = 0;
+                        if(digits[i - 1]) {
+                            digits[i - 1]++;
+                        }else {
+                            digits.splice(0, 0, 1);
+                        }
+                    }
+                }
+                return digits.join('');
+            }else {
+                digits[digits.length - 1]++;
+                return digits.join('');
+            }
+        },
+        moveZeroes(nums) {
+            for(var i = nums.length - 1; i >= 0; i--) {
+                if(nums[i] == 0) {
+                    nums.splice(i, 1);
+                    nums.push(0);
+                }
+            }
+            return nums;
+        },
+        twoSum(nums, target) {
+            var l= nums.length;
+            var n = {};
+            for(var i = 0; i < l; ++i) {
+                if (n[target - nums[i]] !== undefined){
+                    return [n[target-nums[i]], i];
+                }
+                n[nums[i]] = i;
+            }
+        },
+        isValidSudoku(board) {
+            var arr = {}
+            var arr1 = {}
+            for(var i = 0; i < board.length; i++) {
+                var box = Math.floor(i / 3);
+                if(!arr1[box]) {
+                    arr1[box] = {}
+                }
+                for(var j = 0; j < board[i].length; j++) {
+                    var box1 = Math.floor(j / 3);
+                    if(!arr1[box][box1]) {
+                        arr1[box][box1] = [];
+                    }
+                    arr1[box][box1].push(board[i][j]);
+                    if(!arr[j]) {
+                        arr[j] = {}
+                    }
+                    arr[j][i] = board[i][j];
+                    for(var k = 0; k < board[i].length; k++) {
+                        if(j != k && board[i][j] == board[i][k] && board[i][j] != '.') {
+                            console.log(board[i][j], 'board[i][j]')
+                            return false;
+                        }
+                    }
+                }
+            }
+            for(var i in arr) {
+                for(var j in arr[i]) {
+                    for(var k in arr[i]) {
+                        if(j != k && arr[i][j] == arr[i][k] && arr[i][j] != '.') {
+                            return false;
+                        }
+                    }
+                }
+            }
+            for(var i in arr1) {
+                console.log(arr1[i])
+                for(var j in arr1[i]) {
+                    for(var k in arr1[i][j]){
+                       for(var k1 in arr1[i][j]){
+                           if(k != k1 && arr1[i][j][k] == arr1[i][j][k1] && arr1[i][j][k] != '.') {
+                               return false;
+                           }
+                       } 
+                    }
+                }
+            }
+            return true;
+        },
+        rotate2(matrix) {
+            var len = matrix.length;
+            var arr = JSON.parse(JSON.stringify(matrix));
+            for(var i = 0; i < len; i++) {
+                var len1 = matrix[i].length;
+                for(var j = 0; j < len1; j++) {
+                    matrix[i][j] = arr[len1 - j - 1][i];
+                }
+            }
+            return matrix;
         }
     }
 }
